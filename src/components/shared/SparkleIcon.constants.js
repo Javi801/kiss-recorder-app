037100a41f1ee00bec0eb0f36e310997677984f3
Javi801 <javiera.alegria@live.cl>
2026-05-13 20:54:10 -0400

    refactor(SparkleIcon): extract palette constants to dedicated module

---
+++ src/components/shared/SparkleIcon.constants.js
@@ -0,0 +1,8 @@
+export const SPARKLE_PALETTES = ['gold', 'blue', 'pink', 'purple']
+
+export const PALETTE_SWATCHES = {
+  gold:   '#E88C1E',
+  blue:   '#5878C8',
+  pink:   '#D83890',
+  purple: '#7848C8',
+}
